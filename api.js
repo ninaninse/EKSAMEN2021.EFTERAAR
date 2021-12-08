c72a5861d7e2cdb357a9dd4a8c3919a737f4f6ed
ninaninse <90242000+ninaninse@users.noreply.github.com>
2021-12-08 18:08:22 +0100

    Add files via upload

---
+++ api.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../login-system/src/controllers/user-controller');
+
+chai.use(chaiHttp);
+
+describe('API', () => {
+    describe('GET /users', () => {
+        it('should return an array', (done) => {
+            chai
+            .request(app)
+            .get('/users')
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res.status).to.equal(200);
+                expect(res.body).to.be.an('array');
+                expect(res.body.lenght).to.equal(2);
+                done();
+            });
+        });
+    });
+});
